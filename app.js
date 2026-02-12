35555c0bc1bdc946b372a412da2b46aae8392493
Sebastian Novoa <joansnovoaborrero@gmail.com>
2026-02-12 16:03:33 -0500

    Segunda prueba

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Nueva version desplegada');
+  res.end('Nueva version desplegada 2.0');
 });
 
 server.listen(3000, () => {
